96570255f3baabf367c0016b2340b265560affca
DASHAAAAA-5 <d.ryazantseva75@gmail.com>
2025-11-01 15:22:52 +0300

    Delete projects/практич 3 directory

--- projects/практич 3/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Инициализация всех компонентов
-    initBurgerMenu();
-    initSlider();
-    initTabs();
-    initFilter();
-    initAccordion();
-    initModal();
-});
